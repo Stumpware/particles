65de714da0f6663faec1ceff726a4ff734a055b8
Chad Berkley <cberkley@flightstats.com>
2014-10-23 22:27:29 -0700

    fixing rand

--- firmware/particles.cpp
+++ firmware/particles.cpp
@@ -15,7 +15,7 @@ ParticleEmitter::ParticleEmitter(int n) {
     numPixels = n;
     numParticles = MAX_PARTICLES;
     maxVelocity = float(numParticles / 5000.0);
-    stripPosition = (rand(70) + 15) / 100.0;  // 0.0 - 1.0
+    stripPosition = ((rand() * 70) + 15) / 100.0;  // 0.0 - 1.0
     
     for (int i=0; i < MAX_PARTICLES; i++) {
         particles[i] = newParticle();
@@ -25,22 +25,22 @@ ParticleEmitter::ParticleEmitter(int n) {
 particle ParticleEmitter::newParticle() {
     particle p;
     
-    int direction = (rand(2) == 0 ? 1 : -1);
+    int direction = ((rand() * 2) == 0 ? 1 : -1);
 
 //    int maxColor = (rand(2) == 0 ? MAX_COLOR/(rand(35)+5) : 0);
     int maxColor = MAX_COLOR * (1.0 - (rand(50) / 100));
 
-    p.velocity = ((rand(99) + 1) / 100.0) * direction;
+    p.velocity = (((rand() * 99) + 1) / 100.0) * direction;
     
     if (direction > 0) {
-        p.redColor = rand(maxColor);
-        p.greenColor = rand(maxColor*0.25);
-        p.blueColor = rand(maxColor*0.75);
+        p.redColor = rand() * maxColor;
+        p.greenColor = rand() * (maxColor * 0.25);
+        p.blueColor = rand() * (maxColor*0.75);
     }
     else {
-        p.redColor = rand(maxColor);
-        p.greenColor = rand(maxColor*0.25);
-        p.blueColor = rand(maxColor*0.75);
+        p.redColor = rand() * maxColor;
+        p.greenColor = rand() * (maxColor*0.25);
+        p.blueColor = rand() * (maxColor*0.75);
     }
     
     p.startStripPosition = stripPosition;
@@ -59,7 +59,7 @@ particle ParticleEmitter::updateParticle(int i) {
     p->currentStripPosition = p->currentStripPosition +
                               (maxVelocity * p->velocity);  
 
-    p->dimmed = (rand(3) == 0 ? 1 : 0);
+    p->dimmed = (rand() * 3 == 0 ? 1 : 0);
     
     if (0) {
         if (p->currentStripPosition < 0.0)
