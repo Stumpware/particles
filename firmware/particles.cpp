b9fd066b92bfb86d0cac950f22721d6a6c8f90cf
Chad Berkley <cberkley@flightstats.com>
2014-10-23 22:20:39 -0700

    cleaning up

--- firmware/particles.cpp
+++ firmware/particles.cpp
@@ -14,7 +14,7 @@ ParticleEmitter::ParticleEmitter(int n) {
     numPixels = n;
     numParticles = MAX_PARTICLES;
     maxVelocity = float(numParticles / 5000.0);
-    stripPosition = (random(70) + 15) / 100.0;  // 0.0 - 1.0
+    stripPosition = (rand(70) + 15) / 100.0;  // 0.0 - 1.0
     
     for (int i=0; i < MAX_PARTICLES; i++) {
         particles[i] = newParticle();
@@ -24,25 +24,24 @@ ParticleEmitter::ParticleEmitter(int n) {
 particle ParticleEmitter::newParticle() {
     particle p;
     
-    int direction = (random(2) == 0 ? 1 : -1);
+    int direction = (rand(2) == 0 ? 1 : -1);
 
-//    int maxColor = (random(2) == 0 ? MAX_COLOR/(random(35)+5) : 0);
-    int maxColor = MAX_COLOR * (1.0 - (random(50) / 100));
+//    int maxColor = (rand(2) == 0 ? MAX_COLOR/(rand(35)+5) : 0);
+    int maxColor = MAX_COLOR * (1.0 - (rand(50) / 100));
 
-    p.velocity = ((random(99) + 1) / 100.0) * direction;
+    p.velocity = ((rand(99) + 1) / 100.0) * direction;
     
     if (direction > 0) {
-        p.redColor = random(maxColor);
-        p.greenColor = random(maxColor*0.25);
-        p.blueColor = random(maxColor*0.75);
+        p.redColor = rand(maxColor);
+        p.greenColor = rand(maxColor*0.25);
+        p.blueColor = rand(maxColor*0.75);
     }
     else {
-        p.redColor = random(maxColor);
-        p.greenColor = random(maxColor*0.25);
-        p.blueColor = random(maxColor*0.75);
+        p.redColor = rand(maxColor);
+        p.greenColor = rand(maxColor*0.25);
+        p.blueColor = rand(maxColor*0.75);
     }
     
-    p.startTime = millis();
     p.startStripPosition = stripPosition;
     p.currentStripPosition = p.startStripPosition;
     p.dimmed = 0;
@@ -57,10 +56,9 @@ particle ParticleEmitter::updateParticle(int i) {
     particle *p = &particles[i];
     
     p->currentStripPosition = p->currentStripPosition +
-                              (maxVelocity * p->velocity);  // *
-                              //float(millis() - p->startTime);
+                              (maxVelocity * p->velocity);  
 
-    p->dimmed = (random(3) == 0 ? 1 : 0);
+    p->dimmed = (rand(3) == 0 ? 1 : 0);
     
     if (0) {
         if (p->currentStripPosition < 0.0)
